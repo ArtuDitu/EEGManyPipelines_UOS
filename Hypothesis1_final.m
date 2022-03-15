e4c48ea262e892894e547f8defaf2d0ae7130fd8
rzollner <64137446+rzollner@users.noreply.github.com>
2022-03-15 12:01:08 +0100

    add: remove baseline

--- Hypothesis1_final.m
+++ Hypothesis1_final.m
@@ -14,7 +14,7 @@ addpath(genpath('/net/store/nbp/projects/joint_error/EEG_Belt/TFCE/ept_TFCE-matl
 %add triggers
 trigger_manmade = {1010 1011 1019 1020 1021 1029 1030 1031 1039 1040 1041 1049 1090 1091 1099 1110 1111 1119 1120 1121 1129 1190 1191 1199}; 
 trigger_natural={2010 2011 2019 2020 2021 2029 2030 2031 2039 2040 2041 2049 2090 2091 2099 2110 2111 2119 2120 2121 2129 2190 2191 2199};
-window_epoch = [-.1 .2];
+window_epoch = [-.2 .2];
 
 %load data
 
@@ -33,6 +33,10 @@ for eeg_file = 1:size(list_of_files)
     EEG_natural = pop_epoch( EEG, trigger_natural, window_epoch, 'epochinfo', 'yes');
     EEG_manmade = pop_epoch( EEG, trigger_manmade, window_epoch, 'epochinfo', 'yes');
     %eegplot(EEG_manmade.data,'srate',EEG_manmade.srate,'eloc_file',EEG_manmade.chanlocs,'events',EEG_manmade.event)
+    
+    %remove baseline
+    EEG_natural = pop_rmbase(EEG_natural, [-199 0]);
+    EEG_manmade = pop_rmbase(EEG_manmade, [-199 0]);
     
     %average data on epoch level
     EEG_natural.data=mean(EEG_natural.data(:,:,:),3);
@@ -53,9 +57,15 @@ end
 EEG_natural_all=double(permute(EEG_natural_all.data, [3 1 2]));
 EEG_manmade_all=double(permute(EEG_manmade_all.data, [3 1 2]));
 
+% remove VEOG and HEOG
+EEG_natural_all = EEG_natural_all(:,1:70,:);
+EEG_manmade_all = EEG_manmade_all(:,1:70,:);
+
+
+
 %% STEP 3
 % run t-test tfce
-ept_TFCE(EEG_natural_all, EEG_manmade_all, EEG_natural.chanlocs, 'rSample', 512, 'nPerm', 5000, 'E_H', [0.66, 2], 'type', 'd', 'saveName', 'tfce_natural_mammade.mat');
+ept_TFCE(EEG_natural_all, EEG_manmade_all, EEG_natural.chanlocs(1:70), 'rSample', 512, 'nPerm', 5000, 'type', 'd', 'saveName', 'tfce_natural_mammade.mat');
 
 
 %% STEP 4 
@@ -65,12 +75,16 @@ ept_TFCE(EEG_natural_all, EEG_manmade_all, EEG_natural.chanlocs, 'rSample', 512,
 diff=EEG_natural_all-EEG_manmade_all;
 %average over all participants
 diff_mean=mean(diff(:,:,:),1);
-%change structure of data for plotting toolbox
+%reshape data for plotting toolbox
 mean_permuted=double(permute(diff_mean, [2 3 1]));
 
 
 %% STEP 5 
 % plot results 
 % plotting with Ehinger, BV 2018 "EEGVIS toolbox"
+%https://github.com/behinger/eegvis/tree/master/topo_butter
 addpath(genpath('/net/store/nbp/projects/joint_error/EEG_Belt/TFCE/eegvis'));
-plot_topobutter(mean_permuted ,EEG_natural.times,EEG_natural.chanlocs,'pvalues',Results.P_Values, 'colormap',{{'div','RdYlBu'},{'seq','YlGnBu'},'seq'},'topoalpha',0.005)
+load('tfce_natural_mammade.mat')
+plot_topobutter(mean_permuted ,EEG_natural.times,EEG_natural.chanlocs,'pvalues',Results.P_Values, 'colormap',{{'div','RdYlBu'},{'seq','YlGnBu'},'seq'},'topoalpha',0.05)
+
+% ept_TFCE_Toolbox
