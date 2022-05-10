99cb54b46af8ea50499847b4ecce6817899d5687
Artur Czeszumski <aczeszumski@uos.de>
2022-05-10 09:43:36 +0900

    change file name

---
+++ Hypothesis3b.m
@@ -0,0 +1,46 @@
+%Matlab version: R2020a
+% eeglab version: 2020_0
+
+%change the path to the folder with scripts and add the folder to the path
+cd D:\Dropbox\Projects\EEGManyPipelines\Data\final\
+addpath(genpath('D:\Dropbox\Projects\EEGManyPipelines'))
+eeglab;
+
+list_of_files = dir('**/final*');
+
+for eeg_file = 1:size(list_of_files)
+    %load file
+    cd D:\Dropbox\Projects\EEGManyPipelines\Data\final\
+    EEG = pop_loadset(list_of_files(eeg_file).name);
+    EEG = pop_selectevent( EEG, 'type',[1110 1111 1119 2110 2111 2119] ,'renametype','hit','deleteevents','off','deleteepochs','off','invertepochs','off');
+    EEG = pop_selectevent( EEG, 'type',[1120 1121 1129 2120 2121 2129] ,'renametype','miss','deleteevents','off','deleteepochs','off','invertepochs','off');
+    window_epoch=[-.8, 1.3];
+    triggers = {'hit', 'miss'};
+    EEG = pop_epoch( EEG, triggers, window_epoch, 'epochinfo', 'yes');
+    
+    cd D:\Dropbox\Projects\EEGManyPipelines\Data\3a\
+    EEG = pop_saveset(EEG, 'filename',EEG.filename);
+end
+
+ALLEEG = [];
+list_of_files = dir('*.set*');
+for i = 1:size(list_of_files)
+ 
+    EEG = pop_loadset(list_of_files(i).name), 'loadmode', 'info'; 
+    % Store the current EEG to ALLEEG.
+    [ALLEEG, EEG, CURRENTSET] = eeg_store(ALLEEG, EEG, 0);
+end
+
+STUDY=[];
+[STUDY ALLEEG]=std_editset(STUDY, ALLEEG, 'name', 'ERSP_Hypothesis3b',...
+    'task', 'hit_miss', 'filename', 'ERSP_Hypothesis3b.study', 'filepath', ...
+   'D:\Dropbox\Projects\EEGManyPipelines\Data\3a\');
+
+STUDY = std_makedesign(STUDY, ALLEEG, 1, 'name','HitMiss','delfiles','off','defaultdesign','off','variable1','type','values1',{'hit','miss'},'vartype1','categorical','subjselect',{'sub-001','sub-002','sub-003','sub-004','sub-005','sub-006','sub-007','sub-008','sub-009','sub-010','sub-011','sub-012','sub-013','sub-014','sub-015','sub-016','sub-017','sub-018','sub-019','sub-020','sub-021','sub-022','sub-023','sub-024','sub-025','sub-026','sub-027','sub-028','sub-029','sub-030','sub-031','sub-032','sub-033'});
+[STUDY, ALLEEG] = std_precomp(STUDY, ALLEEG, 'channels', 'savetrials','on','interp','off','erp', 'off', 'ersp','on','erspparams',{'cycles',[3 0.8],'nfreqs',100, 'ntimesout',60,'freqs', [3 40]});
+
+STUDY = pop_statparams(STUDY, 'condstats', 'on','method', 'perm');
+
+
+
+std_erspplot(STUDY, ALLEEG, 'channels', {ALLEEG(1).chanlocs(1:72).labels}, 'design', 1, 'plotconditions', 'together', 'timerange', [-200 800], 'alpha',0.05, 'mcorrect', 'fdr');
